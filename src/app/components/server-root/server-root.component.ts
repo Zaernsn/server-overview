6143bb328371a914b389a5b6e4a14c8e94b24619
daniel4971 <daniel4971@gmail.com>
2025-03-11 18:29:12 +0000

    add footer

--- src/app/components/server-root/server-root.component.ts
+++ src/app/components/server-root/server-root.component.ts
@@ -2,19 +2,30 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ServerService } from '../../api/service/server.service';
 import { Server } from '../../api/model/Server';
 import { ServerCardComponent } from '../server-card/server-card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-server-root',
-  imports: [ServerCardComponent],
+  imports: [
+    ServerCardComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+  ],
   templateUrl: './server-root.component.html',
-  styleUrl: './server-root.component.css'
+  styleUrl: './server-root.component.css',
 })
 export class ServerRootComponent implements OnInit {
-  serverService = inject(ServerService)
-  servers: Server[] = []
+  serverService = inject(ServerService);
+  servers: Server[] = [];
+  searchTerm: string = '';
 
   ngOnInit(): void {
-    this.servers = this.serverService.servers
+    this.servers = this.serverService.servers;
   }
-
 }
