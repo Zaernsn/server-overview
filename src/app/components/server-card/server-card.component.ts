1b83dd1b20178a0491b6f82c2d6c251aa9459162
daniel4971 <daniel4971@gmail.com>
2025-03-12 17:29:43 +0000

    add dialog

--- src/app/components/server-card/server-card.component.ts
+++ src/app/components/server-card/server-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Server } from '../../api/model/Server';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,14 +6,32 @@ import { NgClass } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'server-card',
-  imports: [MatCardModule,MatIconModule,MatButtonModule,NgClass,MatFormFieldModule,MatInputModule],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    NgClass,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './server-card.component.html',
-  styleUrl: './server-card.component.css'
+  styleUrl: './server-card.component.css',
 })
 export class ServerCardComponent {
- server = input.required<Server>();
- editMode: boolean = false;
+  server = input.required<Server>();
+
+  editMode: boolean = false;
+  labelCtrl: FormControl = new FormControl('');
+
+  constructor() {
+    effect(() => {
+      this.labelCtrl.setValue(this.server().label);
+    });
+  }
 }
